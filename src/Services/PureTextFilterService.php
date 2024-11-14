22b70d864bdcf86fe8af575ab8744277e302175d
Yasser Elgammal <yasser.elgammal@ymail.com>
2024-11-15 00:26:02 +0200

    Update Pattern For Filtering

--- src/Services/PureTextFilterService.php
+++ src/Services/PureTextFilterService.php
@@ -17,6 +17,16 @@ class PureTextFilterService
         $badWords = Config::get('badwords.words', []);
         $replacement = Config::get('badwords.replacement', '***');
 
-        return str_ireplace($badWords, $replacement, $text);
+        if (empty($badWords)) {
+            return $text; // Return text as is if no bad words are configured
+        }
+
+        $pattern = '/' . implode('|', array_map(function ($word) {
+            return '\b' . implode('[\W_]*', array_map(function ($char) {
+                return $char . '{1,}';
+            }, preg_split('//u', preg_quote($word), -1, PREG_SPLIT_NO_EMPTY))) . '\b';
+        }, $badWords)) . '/iu';
+
+        return preg_replace($pattern, $replacement, $text);
     }
 }
